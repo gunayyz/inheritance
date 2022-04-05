0cf16693c41f0fb755d2f96ef067bb2a89a38fae
gunayyz <100687596+gunayyz@users.noreply.github.com>
2022-04-05 17:04:41 +0400

    Add files via upload

---
+++ Extension/Extension/ExtensionMethods.cs
@@ -0,0 +1,52 @@
+﻿namespace Extension
+{
+    public static class ExtensionMethods
+    {
+
+        public static bool CustomContains(this string word, string str)
+        {
+            if (word.Contains(str))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool CustomContains(this string word, char part)
+        {
+            if (word.Contains(part))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        
+            public static bool IsPrime(this int number)
+            {
+                int count = 0;
+                for (int i = 1; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+    }
+}
+
---
+++ Extension/Extension/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+            Console.WriteLine(number.IsPrime());
+            string word = "C# is an object-oriented programming language.";
+            Console.WriteLine(word.CustomContains("are"));
+            Console.WriteLine(word.CustomContains("c#"));
+            Console.WriteLine(word.CustomContains("programming"));
+        }
+    }
+}
